66b51260662251a10c53d2151fc9b62488ab8ae4
nicole-contreras <nicolecontreras9@icloud.com>
2025-01-24 16:58:29 -0500

    Initial Commit

---
+++ rock-paper-scissors-starter-01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rock-paper-scissors-starter-01
+//
+//  Created by Nicole on 1/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rock-paper-scissors-starter-01/rock_paper_scissors_starter_01App.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissors_starter_01App.swift
+//  rock-paper-scissors-starter-01
+//
+//  Created by Nicole on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissors_starter_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
